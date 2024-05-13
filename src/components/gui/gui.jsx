1318fae0728ea2ebb871673acad434950dfead4f
Amit Mahapatra <connectamit23@gmail.com>
2024-05-13 22:29:12 +0530

    hdgfhgf

--- src/components/gui/gui.jsx
+++ src/components/gui/gui.jsx
@@ -417,7 +417,7 @@ const GUIComponent = (props) => {
                                     <p>
                                         Lorem Ipsum is simply dummy text of the
                                         printing and typesetting industry. Lorem
-                                        Ipsum has been the industry's standard
+                                        Ipsum has been the industry standard
                                         dummy text ever since the 1500s, when an
                                         unknown printer took a galley of type
                                         and scrambled it to make a type specimen
